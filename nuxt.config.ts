8295ff06afe68dd279a04ea2d346306d03bc14fb
Rodrigo Cortés <roco3d@gmail.com>
2022-06-25 00:10:28 -0400

    Initial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    //from Getting Started
+    typescript:{
+        shim:false
+    },
+    modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+    content: {
+        toc: {
+            depth: 3,
+            searchDepth: 3,
+        },
+        highlight: {
+            theme: "dracula-soft",
+        },
+    },
 })
